7c471a145cf8cdb3cfbf001f75db16ea12b703ed
Michael Huang <mihuan@microsoft.com>
2018-06-29 15:54:29 -0700

    Renamed variables for customer arrays

--- Main.java
+++ Main.java
@@ -15,14 +15,14 @@ public class Main {
 }
 
 class ProcessOrder {
-    // Result class has newArrayOfPreferredLines and newArrayOfCustomerLines
+    // Result class has PreferredCustomersLines and Customers Lines
     protected class Result {
-        protected PreferredCustomer[] newArrayOfPreferred;
-        protected Customer[] newArrayOfCustomer;
+        protected PreferredCustomer[] PreferredCustomers;
+        protected Customer[] Customers;
 
-        protected Result(Customer[] newArrayOfCustomer, PreferredCustomer[] newArrayOfPreferred) {
-            this.newArrayOfCustomer = newArrayOfCustomer;
-            this.newArrayOfPreferred = newArrayOfPreferred;
+        protected Result(Customer[] Customers, PreferredCustomer[] PreferredCustomers) {
+            this.Customers = Customers;
+            this.PreferredCustomers = PreferredCustomers;
         }
     }
 
@@ -65,17 +65,17 @@ class ProcessOrder {
                 //If the ID belongs to preferred, call the processPreferred function
                 if (isCustomer < 0 && isPreferred >= 0) {
                     result = processPreferred(arrayOfCustomers, arrayOfPreferred, isPreferred, amountSpent, preferredFileName);
-                    arrayOfCustomers = result.newArrayOfCustomer;
-                    arrayOfPreferred = result.newArrayOfPreferred;
+                    arrayOfCustomers = result.Customers;
+                    arrayOfPreferred = result.PreferredCustomers;
                     // If the ID belongs to customer, call the processCustomer function
                 } else if (isCustomer >= 0 && isPreferred < 0) {
                     result = processCustomer(arrayOfCustomers, arrayOfPreferred, isCustomer, amountSpent);
-                    arrayOfCustomers = result.newArrayOfCustomer;
-                    arrayOfPreferred = result.newArrayOfPreferred;
+                    arrayOfCustomers = result.Customers;
+                    arrayOfPreferred = result.PreferredCustomers;
                 }
             }
-            writeToCustomerFile(result.newArrayOfCustomer, customerFileName);
-            writeToPreferredFile(result.newArrayOfPreferred, preferredFileName);
+            writeToCustomerFile(result.Customers, customerFileName);
+            writeToPreferredFile(result.PreferredCustomers, preferredFileName);
         }
 
         //If the preferred file does not exist or exist but is empty
@@ -93,8 +93,8 @@ class ProcessOrder {
                 int isCustomer = isCustomer(arrayOfCustomers, ID);
                 //Process customer
                 result = processCustomer(arrayOfCustomers, arrayOfPreferred, isCustomer, amountSpent);
-                arrayOfPreferred = result.newArrayOfPreferred;
-                arrayOfCustomers = result.newArrayOfCustomer;
+                arrayOfPreferred = result.PreferredCustomers;
+                arrayOfCustomers = result.Customers;
             }
             writeToPreferredFile(arrayOfPreferred, preferredFileName);
             writeToCustomerFile(arrayOfCustomers, customerFileName);
@@ -156,11 +156,11 @@ class ProcessOrder {
 
             //move the customer from regular to preferred
             arrayOfPreferred = moveToPreferred(arrayOfCustomer[isCustomer], arrayOfPreferred);
-            //PreferredCustomer[] newArrayOfPreferred = readPreferredFile(newArrayOfPreferredLines);
+            //PreferredCustomer[] PreferredCustomers = readPreferredFile(PreferredCustomersLines);
 
             //remove that customer in the regular customer array
             arrayOfCustomer = removePromotedCustomer(arrayOfCustomer, isCustomer);
-            //Customer[] newArrayOfCustomer = readCustomerFile(newArrayOfCustomerLines);
+            //Customer[] Customers = readCustomerFile(CustomersLines);
 
             return new Result(arrayOfCustomer, arrayOfPreferred);
         } else {
@@ -183,12 +183,12 @@ class ProcessOrder {
         arrayOfPreferred[isPreferred].updateAmountSpent(amountAfterDiscount);
         //update the discount percentage
         arrayOfPreferred[isPreferred].updateDiscountPercentage();
-        PreferredCustomer[] newArrayOfPreferred = new PreferredCustomer[arrayOfPreferred.length];
+        PreferredCustomer[] PreferredCustomers = new PreferredCustomer[arrayOfPreferred.length];
         //loop through the array of preferred Customer, and convert each object into string
         for (int count = 0; count < arrayOfPreferred.length; count++) {
-            newArrayOfPreferred[count] = arrayOfPreferred[count];
+            PreferredCustomers[count] = arrayOfPreferred[count];
         }
-        return new Result(arrayOfCustomer, newArrayOfPreferred);
+        return new Result(arrayOfCustomer, PreferredCustomers);
     }
 
     //Remove the promoted customer out of the regular customer array
@@ -196,27 +196,27 @@ class ProcessOrder {
 
         //the new array size decreases by 1
         int newArraySize = arrayOfCustomer.length - 1;
-        Customer[] newArrayOfCustomer = new Customer[newArraySize];
+        Customer[] Customers = new Customer[newArraySize];
 
         //If there is only one customer in the customer file
         if(newArraySize == 0)
-            newArrayOfCustomer = new Customer[0];
+            Customers = new Customer[0];
         //If the removed customer locates at the end of the file
         else if (newArraySize > 0 && isCustomer == arrayOfCustomer.length - 1)
             //copy the old array into a new array except the last customer
-            System.arraycopy(arrayOfCustomer, 0, newArrayOfCustomer, 0, isCustomer);
+            System.arraycopy(arrayOfCustomer, 0, Customers, 0, isCustomer);
             //If the removed customer locates at the beginning of the file
         else if (newArraySize > 0 && isCustomer == 0)
             //start to copy from the second element in the old array into a new array
-            System.arraycopy(arrayOfCustomer, 1, newArrayOfCustomer, 0, newArraySize);
+            System.arraycopy(arrayOfCustomer, 1, Customers, 0, newArraySize);
         //If the removed customer locates in the middle of the file
         else {
             //copy from the beginning of the old array into the new array except the promoted customer
-            System.arraycopy(arrayOfCustomer, 0, newArrayOfCustomer, 0, isCustomer);
-            System.arraycopy(arrayOfCustomer, isCustomer + 1, newArrayOfCustomer, isCustomer,
+            System.arraycopy(arrayOfCustomer, 0, Customers, 0, isCustomer);
+            System.arraycopy(arrayOfCustomer, isCustomer + 1, Customers, isCustomer,
                     newArraySize - isCustomer);
         }
-        return newArrayOfCustomer;
+        return Customers;
     }
 
     //Move to promoted customer to preferred array
@@ -229,17 +229,17 @@ class ProcessOrder {
         // increase the new array size by 1
         if(arrayOfPreferred.length >= 1){
             newArraySize = arrayOfPreferred.length + 1;
-            PreferredCustomer[] newArrayOfPreferred = new PreferredCustomer[newArraySize];
+            PreferredCustomer[] PreferredCustomers = new PreferredCustomer[newArraySize];
             //copy all the preferred customers into a new array
-            System.arraycopy(arrayOfPreferred, 0, newArrayOfPreferred, 0, newArraySize - 1);
+            System.arraycopy(arrayOfPreferred, 0, PreferredCustomers, 0, newArraySize - 1);
             //copy the new promoted customer at the end of the new array
-            newArrayOfPreferred[newArraySize - 1] = newPreferred;
-            return newArrayOfPreferred;
+            PreferredCustomers[newArraySize - 1] = newPreferred;
+            return PreferredCustomers;
         } else {
             newArraySize = 1;
-            PreferredCustomer[] newArrayOfPreferred = new PreferredCustomer[newArraySize];
-            newArrayOfPreferred[0] = newPreferred;
-            return newArrayOfPreferred;
+            PreferredCustomer[] PreferredCustomers = new PreferredCustomer[newArraySize];
+            PreferredCustomers[0] = newPreferred;
+            return PreferredCustomers;
         }
     }
 

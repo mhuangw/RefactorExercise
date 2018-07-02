6338f8c4405f3be7f254e5c5fb91329a3c400851
Michael Huang <mihuan@microsoft.com>
2018-07-02 11:46:16 -0700

    Removed imports from Main class

--- Main.java
+++ Main.java
@@ -1,12 +1,3 @@
-import Customer.*;
-import java.io.IOException;
-import java.io.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
-import static java.lang.Math.pow;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         ProcessOrder processOrder = new ProcessOrder();

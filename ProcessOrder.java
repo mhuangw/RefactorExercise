3a04bbb1e7d6c1469f19a33dbd9a0be89038b2c7
Michael Huang <mihuan@microsoft.com>
2018-07-02 11:45:51 -0700

    Moved imports to ProcessOrder class

--- ProcessOrder.java
+++ ProcessOrder.java
@@ -1,3 +1,12 @@
+import Customer.*;
+import java.io.IOException;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import static java.lang.Math.pow;
+
 public class ProcessOrder {
     // Result class has PreferredCustomersLines and Customers Lines
     protected class Result {

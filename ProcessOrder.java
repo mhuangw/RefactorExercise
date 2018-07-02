f2520afc12f0d544f5d399f57f23555049a1d332
Michael Huang <mihuan@microsoft.com>
2018-07-02 13:07:10 -0700

    Removed some unecessary comments

--- ProcessOrder.java
+++ ProcessOrder.java
@@ -36,19 +36,15 @@ public class ProcessOrder {
 
         Result result;
 
-        //Check whether the preferred exists
         boolean preferredFileExists = preferredFileName.exists();
 
-        //If the preferred file exists
         if (preferredFileExists) {
             PreferredLines = readFileIntoArrayOfLine(preferredFileName);
             PreferredCustomers = readPreferredFile(PreferredLines);
             result = new Result(Customers, PreferredCustomers);
             for (int count = 0; count < TransactionLines.length; count++) {
-                //get customer ID of the transaction
                 int ID = ID(TransactionLines[count]);
 
-                // calculate how much the customer spent
                 double amountSpent = amountSpent(TransactionLines[count]);
 
                 //get the index of the ID in customer or preferred
@@ -71,17 +67,13 @@ public class ProcessOrder {
             writeToPreferredFile(result.PreferredCustomers, preferredFileName);
         }
 
-        //If the preferred file does not exist or exist but is empty
         else if (!preferredFileExists) {
-            //If the preferred file does not exist, create one
             preferredFileName.createNewFile();
             PreferredCustomers = new PreferredCustomer[0];
 
             for (int count = 0; count < TransactionLines.length; count++) {
-                //get customer ID of the transaction
                 int ID = ID(TransactionLines[count]);
 
-                // calculate how much the customer spent
                 double amountSpent = amountSpent(TransactionLines[count]);
                 int isCustomer = isCustomer(Customers, ID);
                 //Process customer
@@ -144,17 +136,9 @@ public class ProcessOrder {
         //Add the current amount spent into the previous amount spent
         arrayOfCustomer[isCustomer].updateAmountSpent(amountSpent);
 
-        //If the customer gets promoted
         if (arrayOfCustomer[isCustomer].isPromoted()) {
-
-            //move the customer from regular to preferred
             PreferredCustomers = moveToPreferred(arrayOfCustomer[isCustomer], PreferredCustomers);
-            //PreferredCustomer[] PreferredCustomers = readPreferredFile(PreferredCustomersLines);
-
-            //remove that customer in the regular customer array
             arrayOfCustomer = removePromotedCustomer(arrayOfCustomer, isCustomer);
-            //Customer[] Customers = readCustomerFile(CustomersLines);
-
             return new Result(arrayOfCustomer, PreferredCustomers);
         } else {
             for (int i = 0; i < arrayOfCustomer.length; i++) {
@@ -168,43 +152,31 @@ public class ProcessOrder {
     //This function is called when the customer ID in the transaction file belongs to preferred customers
     public Result processPreferred(Customer[] arrayOfCustomer, PreferredCustomer[] PreferredCustomers, int isPreferred,
                                  double amountSpent, File fileName) throws IOException {
-        //get the discount percentage
         double discount = PreferredCustomers[isPreferred].getDiscountPercentage();
-        //get the amount spent after discount
         double amountAfterDiscount = amountSpent - (amountSpent * discount);
-        //update the amountSpent
         PreferredCustomers[isPreferred].updateAmountSpent(amountAfterDiscount);
-        //update the discount percentage
         PreferredCustomers[isPreferred].updateDiscountPercentage();
         PreferredCustomer[] PreferredCustomers = new PreferredCustomer[PreferredCustomers.length];
-        //loop through the array of preferred Customer, and convert each object into string
         for (int count = 0; count < PreferredCustomers.length; count++) {
             PreferredCustomers[count] = PreferredCustomers[count];
         }
         return new Result(arrayOfCustomer, PreferredCustomers);
     }
 
-    //Remove the promoted customer out of the regular customer array
     public Customer[] removePromotedCustomer(Customer[] arrayOfCustomer, int isCustomer) {
-
-        //the new array size decreases by 1
         int newArraySize = arrayOfCustomer.length - 1;
         Customer[] Customers = new Customer[newArraySize];
 
-        //If there is only one customer in the customer file
         if(newArraySize == 0)
             Customers = new Customer[0];
         //If the removed customer locates at the end of the file
         else if (newArraySize > 0 && isCustomer == arrayOfCustomer.length - 1)
-            //copy the old array into a new array except the last customer
             System.arraycopy(arrayOfCustomer, 0, Customers, 0, isCustomer);
             //If the removed customer locates at the beginning of the file
         else if (newArraySize > 0 && isCustomer == 0)
-            //start to copy from the second element in the old array into a new array
             System.arraycopy(arrayOfCustomer, 1, Customers, 0, newArraySize);
         //If the removed customer locates in the middle of the file
         else {
-            //copy from the beginning of the old array into the new array except the promoted customer
             System.arraycopy(arrayOfCustomer, 0, Customers, 0, isCustomer);
             System.arraycopy(arrayOfCustomer, isCustomer + 1, Customers, isCustomer,
                     newArraySize - isCustomer);
@@ -212,20 +184,15 @@ public class ProcessOrder {
         return Customers;
     }
 
-    //Move to promoted customer to preferred array
     public PreferredCustomer[] moveToPreferred(Customer customer, PreferredCustomer[] PreferredCustomers) {
         int newArraySize;
         PreferredCustomer newPreferred = new PreferredCustomer(customer.getID(), customer.getFirstName(),
                 customer.getLastName(), customer.getAmountSpent(), 0.0);
-        // get the discount percentage based on the total amountSpent
         newPreferred.updateDiscountPercentage();
-        // increase the new array size by 1
         if(PreferredCustomers.length >= 1){
             newArraySize = PreferredCustomers.length + 1;
             PreferredCustomer[] PreferredCustomers = new PreferredCustomer[newArraySize];
-            //copy all the preferred customers into a new array
             System.arraycopy(PreferredCustomers, 0, PreferredCustomers, 0, newArraySize - 1);
-            //copy the new promoted customer at the end of the new array
             PreferredCustomers[newArraySize - 1] = newPreferred;
             return PreferredCustomers;
         } else {
@@ -236,15 +203,12 @@ public class ProcessOrder {
         }
     }
 
-    //check whether the customer in the transaction belongs to regular customer
     public int isCustomer(Customer[] arrayOfCustomer, int ID) {
         int isCustomer = -1;
         int c = 0;
         do{
             if (ID == arrayOfCustomer[c].getID())
-                //get the index where the ID in transaction matches the ID in regular customer file
                 isCustomer = c;
-                // if the customer is not the the regular customers file, return -1
             else isCustomer = -1;
             c++;
         } while (isCustomer == -1 && c < arrayOfCustomer.length);
@@ -252,22 +216,18 @@ public class ProcessOrder {
         return isCustomer;
     }
 
-    //check whether the customer in the transaction belongs to preferred customers
     public int isPreferred(PreferredCustomer[] arrayOfCustomer, int ID) {
         int isCustomer = -1;
         int c = 0;
         while (isCustomer == -1 && c < arrayOfCustomer.length) {
             if (ID == arrayOfCustomer[c].getID())
-                //get the index where the ID in transaction matches the ID in preferred customer file
                 isCustomer = c;
-                // if the customer is not the the preferred customers file, return -1
             else isCustomer = -1;
             c++;
         }
         return isCustomer;
     }
 
-    //read a file into an array of String
     public String[] readFileIntoArrayOfLine(File filename) throws IOException {
         FileReader fileReader = new FileReader(filename);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -280,7 +240,6 @@ public class ProcessOrder {
         return lines.toArray(new String[lines.size()]);
     }
 
-    //process each line into a customer object
     public Customer[] readCustomerFile(String[] CustomerLines) {
         Customer[] Customers = new Customer[CustomerLines.length];
         for (int count = 0; count < CustomerLines.length; count++) {
@@ -295,7 +254,6 @@ public class ProcessOrder {
         return Customers;
     }
 
-    //process each line into a preferred object
     public static PreferredCustomer[] readPreferredFile(String[] PreferredCustomersCustomerLines) {
         PreferredCustomer[] PreferredCustomers = new PreferredCustomer[PreferredCustomersCustomerLines.length];
         for (int count = 0; count < PreferredCustomersCustomerLines.length; count++) {
@@ -311,7 +269,6 @@ public class ProcessOrder {
         return PreferredCustomers;
     }
 
-    //get the amountSpent for each transaction
     public static double amountSpent(String line) {
         String[] strArray = line.split(" ");
         double radius = Double.parseDouble(strArray[1]);
@@ -320,20 +277,17 @@ public class ProcessOrder {
         double ouncePrice = Double.parseDouble(strArray[4]);
         double squareInchPrice = Double.parseDouble(strArray[5]);
         int quantity = Integer.parseInt(strArray[6]);
-        //calculate the amountSpent
         double amountSpent = priceCalculation(radius, height, ounces, ouncePrice, squareInchPrice, quantity);
 
         return amountSpent;
     }
 
-    //get the customer ID for each transaction
-    public static int ID(String line) {
+    public static int getCustomerID(String line) {
         String[] strArray = line.split(" ");
-        int ID = Integer.parseInt(strArray[0]);
-        return ID;
+        int customerID = Integer.parseInt(strArray[0]);
+        return customerID;
     }
 
-    //calculate the drink and/or design of each transaction
     public static double priceCalculation(double radius, double height, double ounces, double ouncePrice,
                                           double squareInchPrice, int quantity) {
         NumberFormat formatter = new DecimalFormat("##.##");
